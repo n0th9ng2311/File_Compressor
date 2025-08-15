ab0e358240bf1bcd825f6c7b528714f5c8947273
Sudhan <sudhankumargndu@gmail.com>
2025-08-15 15:27:19 +0530

    tiff compression works

--- file_comp/comp_algo/tiff_/tiff_comp.cpp
+++ file_comp/comp_algo/tiff_/tiff_comp.cpp
@@ -1,5 +1,109 @@
-//
-// Created by JOSHI on 16-07-2025.
-//
-
 #include "tiff_comp.h"
+
+
+namespace fs = std::filesystem;
+
+TIFFCompError compressTiff(const std::string& input_path,
+                           const std::string& output_path,
+                           int compression_scheme) {
+
+    //Opening input TIFF
+    TIFF* input_file = TIFFOpen(input_path.c_str(), "r");
+    if (!input_file) {
+        std::cerr << "Could not open input TIFF file" << std::endl;
+        return TIFFCompError::FILE_OPEN_F;
+    }
+
+    fs::path temp_input_path = input_path;
+    fs::path temp_output_path = output_path;
+    
+    if (temp_input_path.extension() != ".tiff" && temp_input_path.extension() != ".TIFF"
+        && temp_input_path.extension() != ".tif" && temp_input_path.extension() != ".TIF") {
+        std::cerr<< "Input file is not a tiff file\n";
+        return TIFFCompError::INVALID_FILE_TYPE;
+    }
+    
+    //Opening output TIFF with compression
+    TIFF* output_file = TIFFOpen(output_path.c_str(), "w");
+    if (!output_file) {
+        TIFFClose(input_file);
+        std::cerr << "Could not create output TIFF file" << std::endl;
+        return TIFFCompError::OUTPUT_F;
+    }
+
+    //Copying tags and set compression
+    std::uint32_t width;
+    std::uint32_t height;
+    std::uint16_t samples;
+    std::uint16_t bits_per_sample;
+    std::uint16_t photometric;
+
+    //Setting input tags
+    if (!TIFFGetField(input_file, TIFFTAG_IMAGEWIDTH, &width)               ||
+        !TIFFGetField(input_file, TIFFTAG_IMAGELENGTH, &height)             ||
+        !TIFFGetField(input_file, TIFFTAG_SAMPLESPERPIXEL, &samples)        ||
+        !TIFFGetField(input_file, TIFFTAG_BITSPERSAMPLE, &bits_per_sample)  ||
+        !TIFFGetField(input_file, TIFFTAG_PHOTOMETRIC, &photometric)) {
+
+        TIFFClose(input_file);
+        TIFFClose(output_file);
+        std::cerr<< "Failed to read one or more required TIFF tags\n";
+
+        return TIFFCompError::TAG_READ_ERR;
+
+        }
+
+    //Setting output tags
+    if (!TIFFSetField(output_file, TIFFTAG_IMAGEWIDTH, width)               ||
+        !TIFFSetField(output_file, TIFFTAG_IMAGELENGTH, height)             ||
+        !TIFFSetField(output_file, TIFFTAG_SAMPLESPERPIXEL, samples)        ||
+        !TIFFSetField(output_file, TIFFTAG_BITSPERSAMPLE, bits_per_sample)  ||
+        !TIFFSetField(output_file, TIFFTAG_PHOTOMETRIC, photometric)        ||
+        !TIFFSetField(output_file, TIFFTAG_COMPRESSION, compression_scheme) ||
+        !TIFFSetField(output_file, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG)) {
+
+        TIFFClose(input_file);
+        TIFFClose(output_file);
+        std::cerr<< "Failed to set output TIFF tags\n";
+        return TIFFCompError::TAG_WRITE_ERR;
+    }
+
+    // Allocate buffer and process image data
+    tmsize_t scanline_size = TIFFScanlineSize(input_file);
+    std::uint8_t* buf = (std::uint8_t*)_TIFFmalloc(scanline_size);
+    if (!buf) {
+        TIFFClose(input_file);
+        TIFFClose(output_file);
+        std::cerr<< "Failed to allocate memory for buffer\n";
+        return TIFFCompError::MEM_ALLOC_F;
+    }
+
+    for (std::uint32_t row = 0; row < height; ++row) {
+        if (TIFFReadScanline(input_file, buf, row) == -1) {
+            TIFFClose(input_file);
+            TIFFClose(output_file);
+            std::cerr << "Error reading scanline " << row << "\n";
+            return TIFFCompError::FILE_READING_F;
+        }
+        if (TIFFWriteScanline(output_file, buf, row) == -1) {
+            TIFFClose(input_file);
+            TIFFClose(output_file);
+            std::cerr << "Error writing scanline " << row << "\n";
+            return TIFFCompError::FILE_WRITING_F;
+        }
+    }
+
+    _TIFFfree(buf);
+    TIFFClose(input_file);
+    TIFFClose(output_file);
+
+    auto inputF_size = fs::file_size(input_path);
+    auto outputF_size = fs::file_size(output_path);
+    double reduction_percent = (static_cast<double>(outputF_size) / inputF_size) * 100.0;
+
+    std::cout<< "\nOriginal Size: "<<inputF_size << " bytes"
+             << "\nCompressed Size: "<<outputF_size << " bytes"
+             << "\nSize reduced to: "<< reduction_percent <<"% of original ";
+
+    return TIFFCompError::SUCCESS;
+}
---
+++ file_comp/comp_algo/tiff_/tiff_comp.h
@@ -0,0 +1,26 @@
+#ifndef  TIFF_COMP_H
+#define TIFF_COMP_H
+
+#include"../../hdr/common.h"
+#include"tiffio.h"
+
+
+enum class TIFFCompError {
+    SUCCESS,
+    FILE_OPEN_F,
+    INVALID_FILE_TYPE,
+    TAG_READ_ERR,
+    TAG_WRITE_ERR,
+    FILE_READING_F,
+    FILE_WRITING_F,
+    MEM_ALLOC_F,
+    OUTPUT_F,
+    UNSUPPORTED_FORMAT
+};
+
+TIFFCompError compressTiff(const std::string& input_path,
+                           const std::string& output_path,
+                           int compression_scheme = COMPRESSION_ADOBE_DEFLATE);
+
+
+#endif
