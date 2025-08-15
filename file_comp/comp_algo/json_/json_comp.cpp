7073ebca933ba2e5fab54ad95d1cfd226e5bde51
Sudhan <sudhankumargndu@gmail.com>
2025-08-15 13:55:17 +0530

    json to json.gz works

---
+++ file_comp/comp_algo/json_/json_comp.cpp
@@ -0,0 +1,75 @@
+
+#include "json_comp.h"
+
+namespace fs = std::filesystem;
+JSONCompError compressJSON(const std::string& input_path,
+                        const std::string& output_path) {
+
+    //Verifying if the file exists and if it is a xml
+    if (!fs::exists(input_path)) {
+        std::cerr << "Input file not exists\n";
+        return JSONCompError::FILE_OPEN_F;
+    }
+
+    fs::path temp_input_path = input_path;
+    fs::path temp_output_path = output_path;
+    if ( temp_input_path.extension()!= ".json" && temp_input_path.extension() != ".JSON") {
+        std::cerr << "Input file is not a json file\n";
+        return JSONCompError::INVALID_FILE_TYPE;
+    }
+
+    //Reading the xml file
+    std::ifstream input_file(input_path, std::ios::binary);
+    if (!input_file.is_open()) {
+        std::cerr << "Input file is not open\n";
+        return JSONCompError::FILE_OPEN_F;
+    }
+
+    auto file_size = fs::file_size(input_path);
+    std::vector<char> input_data(file_size);
+
+    if (!input_file.read(input_data.data(), file_size)) {
+        std::cerr << "Input file is not read\n";
+        return JSONCompError::FILE_READING_F;
+    }
+
+    uLongf compressed_size = compressBound(input_data.size());
+    std::vector<Bytef> compressed_data(compressed_size);
+
+    //prepare for compression
+    auto result = compress2(compressed_data.data(),
+                                &compressed_size,
+                                reinterpret_cast<const Bytef*>(input_data.data()), file_size,
+                            Z_BEST_COMPRESSION);
+
+    if (result != Z_OK) {
+        std::cerr<< "Compression failed with error code: " << result<<"\n";
+        return JSONCompError::ENCODING_F;
+    }
+
+    //Writing compressed data
+    fs::create_directories(temp_output_path.parent_path()); //creating output dir if needed
+
+    std::ofstream output_file(output_path, std::ios::binary);
+    if (!output_file.is_open()) {
+        std::cerr<< "Error opening the output file\n";
+        return JSONCompError::OUTPUT_F;
+    }
+
+    if (!output_file.write(reinterpret_cast<char*>(compressed_data.data()), compressed_size)) {
+        std::cerr<< "Error writing to the output file\n";
+        return JSONCompError::FILE_WRITING_F;
+    }
+
+    output_file.close();
+
+    auto inputF_size = fs::file_size(input_path);
+    auto outputF_size = fs::file_size(output_path);
+    double reduction_percent = (static_cast<double>(outputF_size) / inputF_size) * 100.0;
+
+    std::cout<< "\nOriginal Size: "<<inputF_size << " bytes"
+             << "\nCompressed Size: "<<outputF_size << " bytes"
+             << "\nSize reduced to: "<< reduction_percent <<"% of original ";
+
+    return JSONCompError::SUCCESS;
+}
---
+++ file_comp/comp_algo/json_/json_comp.h
@@ -0,0 +1,25 @@
+#ifndef JSON_COMP_H
+#define JSON_COMP_H
+
+#include"../../hdr/common.h"
+#include "zlib.h"
+
+
+
+enum class JSONCompError {
+    SUCCESS,
+    FILE_OPEN_F,
+    INVALID_FILE_TYPE,
+    FILE_READING_F,
+    FILE_WRITING_F,
+    ENCODING_F,
+    OUTPUT_F,
+    UNSUPPORTED_FORMAT
+};
+
+JSONCompError compressJSON(const std::string& input_path,
+                        const std::string& output_path);
+
+
+
+#endif
