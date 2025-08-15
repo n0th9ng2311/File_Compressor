94408c3528fb9c443ebe25841b6ab9f4fbe4db12
Sudhan <sudhankumargndu@gmail.com>
2025-08-15 09:42:19 +0530

    Added better error checking to log

--- file_comp/comp_algo/log_/log_comp.cpp
+++ file_comp/comp_algo/log_/log_comp.cpp
@@ -1,34 +1,37 @@
 #include "log_comp.h"
 
+#include "../csv_/csv_comp.h"
+
 //This function is absolutely similar to the one at csv comp, they both can be compressed using compress2
 namespace fs = std::filesystem;
-bool compressLOG(const std::string& input_path,
+LOGCompError compressLOG(const std::string& input_path,
                 const std::string& output_path) {
+
     //Verifying if the file exists and if it is a log
     if (!fs::exists(input_path)) {
         std::cerr << "Input file not exists\n";
-        return false;
+        return LOGCompError::FILE_READING_F;
     }
 
     fs::path temp_input_path = input_path;
     fs::path temp_output_path = output_path;
     if ( temp_input_path.extension()!= ".log" && temp_input_path.extension() != ".LOG") {
         std::cerr << "Input file is not a log file\n";
-        return false;
+        return LOGCompError::INVALID_FILE_TYPE;
     }
 
     //Reading the log file
     std::ifstream input_file(input_path, std::ios::binary);
     if (!input_file.is_open()) {
         std::cerr << "Input file is not open\n";
-        return false;
+        return LOGCompError::FILE_OPEN_F;
     }
 
     auto file_size = fs::file_size(input_path);
     std::vector<char> input_data(file_size);
     if (!input_file.read(input_data.data(), file_size)) {
         std::cerr<< "Failed to read input file\n";
-        return false;
+        return LOGCompError::FILE_READING_F;
     }
 
     //Preparing for compression
@@ -43,7 +46,7 @@ bool compressLOG(const std::string& input_path,
 
     if (result != Z_OK) {
         std::cerr<< "Compression failed, error: "<< result<< "\n";
-        return false;
+        return LOGCompError::ENCODING_F;
     }
 
     //Writing compressed data
@@ -52,12 +55,12 @@ bool compressLOG(const std::string& input_path,
     std::ofstream output_file(output_path, std::ios::binary);
     if (!output_file.is_open()) {
         std::cerr<< "Error opening the output file\n";
-        return false;
+        return LOGCompError::OUTPUT_F;
     }
 
     if (!output_file.write(reinterpret_cast<char*>(compressed_data.data()), compressed_size)) {
         std::cerr<< "Error writing to the output file\n";
-        return false;
+        return LOGCompError::FILE_WRITING_F;
     }
 
     output_file.close();
@@ -70,7 +73,7 @@ bool compressLOG(const std::string& input_path,
              << "\nCompressed Size: "<<outputF_size << " bytes"
              << "\nSize reduced to: "<< reduction_percent <<"% of original ";
 
-    return true;
+    return LOGCompError::SUCCESS;
 }
 
 
--- file_comp/comp_algo/log_/log_comp.h
+++ file_comp/comp_algo/log_/log_comp.h
@@ -4,6 +4,17 @@
 #include"..\..\hdr\common.h"
 #include"zlib.h"
 
-bool compressLOG(const std::string& input_path,
+enum class LOGCompError {
+    SUCCESS,
+    FILE_OPEN_F,
+    INVALID_FILE_TYPE,
+    FILE_READING_F,
+    FILE_WRITING_F,
+    ENCODING_F,
+    OUTPUT_F,
+    UNSUPPORTED_FORMAT
+};
+
+LOGCompError compressLOG(const std::string& input_path,
                 const std::string& output_path);
 #endif
